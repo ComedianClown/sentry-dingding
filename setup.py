0b981dd50ce98fde2d96408474928ccaafcf6781
unknown <chenyoujian@GrandBlue.GF.com>
2019-01-30 14:23:33 +0800

    fix unicode encode error

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="sentry-dingding",
-    version='0.0.2',
+    version='0.0.21',
     author='ansheng',
     author_email='ianshengme@gmail.com',
     url='https://github.com/anshengme/sentry-dingding',
